36e096e9a71bbed302b854c8c21c724cfcbe2593
jed4739 <tjsdlr04@gmail.com>
2022-10-06 09:25:55 +0900

    first

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,8 +1,8 @@
 package hello.hellospring.controller;
 
 import org.springframework.boot.Banner;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
