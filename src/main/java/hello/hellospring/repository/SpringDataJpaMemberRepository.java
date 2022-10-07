e4dd2426062df1daafa1190adad1ecfae9f9f593
jed4739 <tjsdlr04@gmail.com>
2022-10-07 16:59:24 +0900

    error last hell start

--- src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
+++ src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -6,6 +6,5 @@ import java.util.Optional;
 
 public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
 
-    @Override
     Optional<Member> findByName(String name);
 }
--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -20,10 +20,17 @@ public class MemberService {
 
     // 회원가입
     public Long join(Member member) {
-        // 같은 이름이 있는 중복 회원
-        validateDuplicateMember(member);
-        memberRepository.save(member);
-        return  member.getId();
+
+        long start = System.currentTimeMillis();
+        try {
+            validateDuplicateMember(member);
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join = " + timeMs + "ms");
+        }
     }
 
     private void validateDuplicateMember(Member member) {
@@ -35,9 +42,18 @@ public class MemberService {
 
     // 전체 회원 조회
     public List<Member> findMembers() {
-        return memberRepository.findAll();
+        long start = System.currentTimeMillis();
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers " + timeMs + "ms");
+        }
+
     }
     public Optional<Member> findOne(Long memberId) {
         return memberRepository.findById(memberId);
     }
+
 }
